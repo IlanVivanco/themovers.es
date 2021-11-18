43bb1ae06c4b7f208f826d6bca5e37a496a27940
Ilán Vivanco <ilanvivanco@gmail.com>
2021-11-18 15:53:31 +0100

    Limit datepicker dates to future dates only

--- js/main.js
+++ js/main.js
@@ -8,4 +8,17 @@ jQuery(function ($) {
 	$(document).on('gform_post_render', function (event, form_id, current_page) {
 		$('.tooltip').powerTip();
 	});
+
+	// Limit the dates from today to one year to the future
+	gform.addFilter('gform_datepicker_options_pre_init', function (optionsObj, formId, fieldId) {
+		//only apply to quote form
+		if (formId != 1) return optionsObj;
+
+		if (fieldId == 7) {
+			optionsObj.minDate = 0;
+			optionsObj.yearRange = '-1:+1';
+		}
+
+		return optionsObj;
+	});
 });
