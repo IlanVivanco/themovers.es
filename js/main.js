ef1f465f742d778b7f9606128d619eedeec603b4
Ilán Vivanco <ilanvivanco@gmail.com>
2022-02-22 14:05:57 +0100

    Fix form events

--- js/main.js
+++ js/main.js
@@ -68,24 +68,56 @@ jQuery(function ($) {
 			return total;
 		});
 
+		// Track the submission form events
+		$(document).on('gform_confirmation_loaded', function (event, formId) {
+			if (formId == 1) {
+				const service = $('.cart-total__price-title').text();
+				const total = parseFloat($('.cart-total__price-amount').text().replace(',', '.'));
+
+				dataLayer.push({ ecommerce: null });
+				dataLayer.push({
+					event: 'purchase',
+					ecommerce: {
+						transaction_id: uuid(),
+						value: 9.99,
+						currency: 'EUR',
+						coupon: '',
+						items: [
+							{
+								item_name: 'TheMovers',
+								price: 9.99,
+								quantity: 1,
+							},
+						],
+					},
+				});
+			}
+		});
+
 		// Track the quote form events
 		$(document).on('gform_page_loaded', function (event, formId, currentPage) {
 			if (formId == 1) {
-				trackEvent('form', { step: currentPage });
+				dataLayer.push({
+					event: 'form',
+					step: currentPage,
+				});
 
 				if (currentPage == 3) {
 					setTimeout(() => {
 						const service = $('.cart-total__price-title').text();
-						const total = $('.cart-total__price-amount').text();
+						const total = parseFloat($('.cart-total__price-amount').text().replace(',', '.'));
 
-						trackEvent('add_to_cart', {
+						dataLayer.push({ ecommerce: null });
+						dataLayer.push({
+							event: 'add_to_cart',
 							ecommerce: {
+								value: total,
+								currency: 'EUR',
 								items: [
 									{
 										item_name: service,
 										price: total,
 										quantity: 1,
-										currency: 'EUR',
 									},
 								],
 							},
@@ -96,13 +128,13 @@ jQuery(function ($) {
 		});
 	}
 
-	// Send events to Google Tag Manager
-	function trackEvent(event, data) {
-		if (typeof window.dataLayer != 'undefined') {
-			window.dataLayer.push({
-				event,
-				...data,
-			});
-		}
+	function uuid() {
+		var dt = new Date().getTime();
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+			var r = (dt + Math.random() * 16) % 16 | 0;
+			dt = Math.floor(dt / 16);
+			return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+		});
+		return uuid;
 	}
 });
